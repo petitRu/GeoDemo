ce6dfd699f1b578d46a727e9137bd95b2fce8a98
unknown <rogerdominguezt@gmail.com>
2014-10-16 15:41:49 +0200

    second commit: backgroundTask Service

--- geoDemo/MainPage.xaml.cs
+++ geoDemo/MainPage.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿#define DEBUG_AGENT
+using System;
 using System.Windows;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Scheduler;
+using Microsoft.Phone.Shell;
 namespace geoDemo
 {
+   
     public partial class MainPage : PhoneApplicationPage
     {
        
@@ -13,6 +17,57 @@ namespace geoDemo
         {
             InitializeComponent();
             w = new WorkThreadFunctionThread();
+
+
+           
+
+            LaunchTask();
+        }
+
+        private void LaunchTask()
+        {
+
+
+            try
+            {
+
+           
+
+
+            PeriodicTask task;
+            task = ScheduledActionService.Find("Service") as PeriodicTask;
+            bool found = (task != null);
+            if (!found)
+            {
+                task = new PeriodicTask("Service");
+            }
+            task.Description = "Aplicació gencat geolocalització";
+            task.ExpirationTime = DateTime.Now.AddDays(10);
+            if (!found)
+            {
+                ScheduledActionService.Add(task);
+            }
+            else
+            {
+                ScheduledActionService.Remove("Service");
+                ScheduledActionService.Add(task);
+            }
+
+
+
+                //ScheduledActionService.LaunchForTest("GeoService", TimeSpan.FromSeconds(5));
+#if DEBUG_AGENT
+            ScheduledActionService.LaunchForTest("Service", TimeSpan.FromSeconds(5));
+#endif
+            }
+            catch (InvalidOperationException e)
+            {
+                System.Diagnostics.Debug.WriteLine("TASK  FIRST " + e.Message);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("TASK  second " + e.Message);
+            }
         }
 
         private void startLocationButton_Click(object sender, RoutedEventArgs e)
